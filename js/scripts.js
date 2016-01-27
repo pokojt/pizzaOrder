d5ed4e5767110743281932414b1a61f16a93b417
Taylor Pokoj <pokojt@gmail.com>
2016-01-26 19:58:31 -0800

    Add first test spec and pass for PizzaOrder based on quantity, size, and toppings.

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,32 @@
-function PizzaOrder() {
+function PizzaOrder(quantity, pizzaSize, toppings) {
+  this.quantity = quantity;
+  this.pizzaSize = pizzaSize;
+  this.toppings = toppings;
+}
 
 
 
-$(document).ready(function() {
-  $("form#new-order").submit(function(event) {
-    event.preventDefault();
-
-    var inputtedQuantity = $("input#inputtedQuantity").val();
-    var inputtedSize = $("input#inputtedSize").val();
-    var inputtedToppings = $("input#inputtedToppings").val();
-    var newPizzaOrder = new PizzaOrder (inputtedQuantity, inputtedSize, inputtedToppings);
-
-    $("ul#orders").append("<li><span class='order'>" + newPizzaOrder.fullOrder() + "</span></li>");
-
-    $("input#inputtedQuantity").val("");
-    $("input#inputtedSize").val("");
-    $("input#inputtedToppings").val("");
-
-    $(".order").last().click(function() {
-      $("#show-order").show();
-      $("#show-order h2").text(newPizzaOrder.fullName());
-      $(".quantity").text(newPizzaOrder.firstName);
-      $(".size").text(newPizzaOrder.lastName);
-    });
-  });
-
-});
+// $(document).ready(function() {
+//   $("form#new-order").submit(function(event) {
+//     event.preventDefault();
+//
+//     var inputtedQuantity = $("input#inputtedQuantity").val();
+//     var inputtedSize = $("input#inputtedSize").val();
+//     var inputtedToppings = $("input#inputtedToppings").val();
+//     var newPizzaOrder = new PizzaOrder (inputtedQuantity, inputtedSize, inputtedToppings);
+//
+//     $("ul#orders").append("<li><span class='order'>" + newPizzaOrder.fullOrder() + "</span></li>");
+//
+//     $("input#inputtedQuantity").val("");
+//     $("input#inputtedSize").val("");
+//     $("input#inputtedToppings").val("");
+//
+//     $(".order").last().click(function() {
+//       $("#show-order").show();
+//       $("#show-order h2").text(newPizzaOrder.fullName());
+//       $(".quantity").text(newPizzaOrder.firstName);
+//       $(".size").text(newPizzaOrder.lastName);
+//     });
+//   });
+//
+// });
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,16 @@
-describe ('PizzaOrder', function(){
+describe ('PizzaOrder', function() {
   it("creates a new pizza order with the given properties", function() {
-    var 
+    var testOrder = new PizzaOrder("1", "Large", "cheese");
+    expect(testOrder.quantity).to.equal("1");
+    expect(testOrder.pizzaSize).to.equal("Large");
+    expect(testOrder.toppings).to.equal("cheese");
+  });
 
+  // it("adds the price method to the order", function() {
+  //   var testOrder = new PizzaOrder("1", "Large", "cheese");
+  //   expect(testOrder.price()).to.equal(16);
+  // });
 });
+
+
+//just cheese small: 12, medium: 14, large: 16, xl: 18
