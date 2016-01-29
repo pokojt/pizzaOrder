0d3565866bb0c5d98df43e75e8780dd4b9c02468
Taylor Pokoj <pokojt@gmail.com>
2016-01-29 09:06:39 -0800

    change order summary method to show pizza name instead of toppings.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
-function PizzaOrder(quantity, pizzaSize, toppings, price) {
+function PizzaOrder(quantity, pizzaSize, pizzaName, toppings, price) {
   this.quantity = quantity;
   this.pizzaSize = pizzaSize;
+  this.pizzaName = pizzaName;
   this.toppings = [];
   this.price = price;
 };
@@ -32,38 +33,32 @@ PizzaOrder.prototype.calculatePrice = function() {
 };
 
 PizzaOrder.prototype.orderSummary = function() {
-  // debugger;
-  for (var i = 0; i < specialToppings.length; i++) {
-    if(this.toppings[i] === specialToppings[i]) {
-      chosenToppings.push(specialToppings[i]);
-    }
-  return this.pizzaSize + " " + this.toppings[i];
-  }
+  return this.pizzaSize + " " + this.pizzaName;
 };
 
 
 $(document).ready(function() {
   $("form#new-order").submit(function(event) {
     event.preventDefault();
-    debugger;
 
 
-    var inputtedQuantity = $("input#inputtedQuantity").val();
-    var inputtedSize = $("select#inputtedSize").val();
+    var inputtedQuantity = $(this).find("input#inputtedQuantity").val();
+    var inputtedSize = $(this).find("select#inputtedSize").val();
+    var inputtedName = $(this).find("input#inputtedName").val();
 
     var inputtedToppings = [$("input[name=JustCheese]:checked").val(), $("input[name=ExtraCheese]:checked").val(), $("input[name=ExtraSauce]:checked").val(), $("input[name=Garlic]:checked").val(), $("input[name=RoastedPeppers]:checked").val(), $("input[name=Pepperoni]:checked").val(), $("input[name=Sausage]:checked").val(), $("input[name=Spinach]:checked").val(), $("input[name=Pineapple]:checked").val(),
     $("input[name=CanadianBacon]:checked").val(), $("input[name=Bacon]:checked").val(),
-    $("input[name=Onions]:checked").val(), $("input[name=Mushrooms]:checked").val()]; ///got from ronando form_dating
+    $("input[name=Onions]:checked").val(), $("input[name=Mushrooms]:checked").val()];
 
-    var newPizzaOrder = new PizzaOrder (inputtedQuantity, inputtedSize, inputtedToppings);
+    var newPizzaOrder = new PizzaOrder (inputtedQuantity, inputtedSize, inputtedName, inputtedToppings);
 
-
+    $(".orderList").show();
     $("ul#orders").append("<li><span class='order'>" + newPizzaOrder.orderSummary() + "</span></li>");
 
 //clears the input fields after submit
-    $("input.inputtedQuantity").val("");
-    $("input.inputtedSize").val("");
-    $("input.inputtedToppings").val("");
+    $("input#inputtedQuantity").val("");
+    $("select#inputtedSize").val("");
+    $('input:checkbox').removeAttr('checked');
 
 //when you click the appended order, it shows up on the side with more details
     $(".order").last().click(function() {
